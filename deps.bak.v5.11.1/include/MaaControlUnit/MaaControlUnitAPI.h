9de2be36794f694ede3f2ebaec3df728e3456581
okami-horo <202130310192@stu.shmtu.edu.cn>
2026-09-19 13:45:43 +0800

    chore: 备份 v5.11.1 依赖文件
    将 MaaFramework v5.11.1 的 LICENSE.md 和 README.md 备份至 deps.bak.v5.11.1 目录

---
+++ deps.bak.v5.11.1/include/MaaControlUnit/MaaControlUnitAPI.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "MaaControlUnitDef.h" // IWYU pragma: export
+
+#include "ControlUnitAPI.h"
+#include "RecordTypes.h"
+
+#include "AdbControlUnitAPI.h"
+#include "AndroidNativeControlUnitAPI.h"
+#include "CustomControlUnitAPI.h"
+#include "DbgControlUnitAPI.h"
+#include "GamepadControlUnitAPI.h"
+#include "KWinControlUnitAPI.h"
+#include "MacOSControlUnitAPI.h"
+#include "PlayCoverControlUnitAPI.h"
+#include "RecordControlUnitAPI.h"
+#include "ReplayControlUnitAPI.h"
+#include "Win32ControlUnitAPI.h"
+#include "WlRootsControlUnitAPI.h"
